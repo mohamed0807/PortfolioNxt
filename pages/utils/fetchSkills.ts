e801535cf12db6e815cea935237a14d56c6609d3
mohamed0807 <122071103+mohamed0807@users.noreply.github.com>
2023-01-24 11:24:02 +0530

    Update fetchSkills.ts

--- pages/utils/fetchSkills.ts
+++ pages/utils/fetchSkills.ts
@@ -1,4 +1,6 @@
 import { Skill } from "../../typing";
+import React from 'react'
+
 export const fetchSkill = async () => {
   // const res = await fetch(`http://localhost:3000/api/getSkills`);
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
