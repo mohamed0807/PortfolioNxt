773ecf00d95e4bf73d517d1ef68a54fd780aba19
mohamed0807 <122071103+mohamed0807@users.noreply.github.com>
2023-01-24 11:23:48 +0530

    Update fetchProjects.ts

--- pages/utils/fetchProjects.ts
+++ pages/utils/fetchProjects.ts
@@ -1,4 +1,6 @@
 import { Project } from "../../typing";
+import React from 'react'
+
 export const fetchProject = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`
