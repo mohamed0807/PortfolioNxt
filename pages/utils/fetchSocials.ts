5120cf1b375feb81751aab00ffb9dbc92106fe5d
mohamed0807 <122071103+mohamed0807@users.noreply.github.com>
2023-01-24 11:24:17 +0530

    Update fetchSocials.ts

--- pages/utils/fetchSocials.ts
+++ pages/utils/fetchSocials.ts
@@ -1,4 +1,6 @@
 import { Social } from "../../typing";
+import React from 'react'
+
 export const fetchSocial = async () => {
   // const res = await fetch(`http://localhost:3000/api/getSocials`);
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
