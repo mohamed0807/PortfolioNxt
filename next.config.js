acba712d3dbd50b2dea7f144a9f0208e594074a8
mohamed0807 <122071103+mohamed0807@users.noreply.github.com>
2023-01-24 11:27:41 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains:["cdn.sanity.io","i.pinimg.com"]
-  }
+  },
+  eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 }
 
 module.exports = nextConfig
